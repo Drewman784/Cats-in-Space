c29f1bf1004f7172ca45bc27b5345bec83db8a60
caruiter <99144307+caruiter@users.noreply.github.com>
2025-02-25 00:04:08 -0500

    followed tutorial through 9-16
    - created class SampleUnit
    - created two unit prefabs
    - created two materials for units
    - created rendering layers for background and units
    
    note: the instructions said to put "cubes" in for the units appearance but I used a square sprite instead. possibly a mistake, hopefully easily fixed if so

---
+++ Assets/Scripts/SampleUnit.cs
@@ -0,0 +1,38 @@
+using TbsFramework.Units;
+using UnityEngine;
+
+public class SampleUnit : Unit
+{
+    //this code is borrowed from the tutorial in the documentation
+    public Color LeadingColor;
+    public override void Initialize()
+    {
+        base.Initialize();
+        GetComponentInChildren<Renderer>().material.color = LeadingColor;
+    }
+
+    public override void MarkAsFriendly()
+    {
+        GetComponentInChildren<Renderer>().material.color  = new Color (0.8f,1,0.8f);
+    }
+
+    public override void MarkAsReachableEnemy()
+    {
+        GetComponentInChildren<Renderer>().material.color = Color.red;
+    }
+
+    public override void MarkAsSelected()
+    {
+        GetComponentInChildren<Renderer>().material.color = Color.green;
+    }
+
+    public override void MarkAsFinished()
+    {
+        GetComponentInChildren<Renderer>().material.color = Color.gray;
+    }
+
+    public override void UnMark()
+    {
+        GetComponentInChildren<Renderer>().material.color = LeadingColor;
+    }
+}
