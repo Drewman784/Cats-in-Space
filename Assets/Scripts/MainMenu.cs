e10b1bac8b2e4e7b52724b71fba35796af695f26
Drew <technoman1@icloud.com>
2025-03-02 14:15:38 -0500

    Main Menu Implementation

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using TbsFramework.Grid;
+using TbsFramework.Grid.GridStates;
+using TbsFramework.Players;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace TbsFramework.Gui
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public Button ExitButton;
+        public Button NewGameButton;
+        public Button LoadGameButton;
+        public Button SkirmishButton;
+        public Button OptionsButton;
+        public Button CreditButton;
+
+        public void Skirmish()
+        {
+            SceneManager.LoadScene("TestingScene");
+        }
+        public void NewGame()
+        {
+            SceneManager.LoadScene("Level_One");
+        }
+        public void ExitGame()
+        {
+            Application.Quit();
+        }
+
+        void Awake()
+        {
+            
+        }
+
+        //private void OnLevelLoading(object sender, EventArgs e)
+        //{
+        //    Debug.Log("Level is loading");
+        //}
+        //
+        //private void OnLevelLoadingDone(object sender, EventArgs e)
+        //{
+        //    Debug.Log("Level loading done");
+        //    Debug.Log("Press 'm' to end turn");
+        //}
+
+        void Update()
+        {
+
+        }
+    }
+}
