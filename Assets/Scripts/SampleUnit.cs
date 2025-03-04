0d5f6d9b7d9abfc899f5127a36703c4101b16658
caruiter <99144307+caruiter@users.noreply.github.com>
2025-03-03 20:49:55 -0500

    basic unit health ui drafted + implemented
    - tweaked placeholder unit prefab designs
    - added ui indicating health and action points
    - implemented with code
    - added another fix for same attack range bug as last time

--- Assets/Scripts/SampleUnit.cs
+++ Assets/Scripts/SampleUnit.cs
@@ -1,6 +1,9 @@
+using Microsoft.Unity.VisualStudio.Editor;
 using TbsFramework.Units;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class SampleUnit : Unit
 {
@@ -11,12 +14,22 @@ public class SampleUnit : Unit
     public bool isStructure;
     Color friendlyCustomColor;
 
+    private GameObject healthBar;
+    private GameObject actionMarker;
+
     public override void Initialize()
     {
         base.Initialize();
         GetComponentInChildren<Renderer>().material.color = LeadingColor;
         transform.localPosition += Offset;
         friendlyCustomColor = LeadingColor + new Color(0f,0f,0f,0.5f);
+        healthBar = transform.GetChild(2).transform.GetChild(0).transform.GetChild(1).gameObject;
+        actionMarker = transform.GetChild(2).transform.GetChild(1).gameObject;
+        if(this.PlayerNumber!=0){
+            UnityEngine.UI.Image hBar = healthBar.GetComponent<UnityEngine.UI.Image>();
+            hBar.color = Color.red; 
+            actionMarker.SetActive(false);
+        }
     }
 
     public override void MarkAsFriendly()
@@ -38,16 +51,30 @@ public class SampleUnit : Unit
     public override void MarkAsFinished()
     {
         GetComponentInChildren<Renderer>().material.color = Color.gray;
+        actionMarker.SetActive(false);
     }
 
     public override void UnMark()
     {
         //Debug.Log("Unmark");
-        GetComponentInChildren<Renderer>().material.color = LeadingColor;
+        if(this.HitPoints>0){
+            GetComponentInChildren<Renderer>().material.color = LeadingColor;
+            if(this.PlayerNumber==0){
+                actionMarker.SetActive(true);
+            }
+        }
     }
 
-    public void OnMouseOver() {
-        //Debug.Log("mouse over!!");
+    public void ShowHealth(){ //updates healthbar
+        float barHealth = (float)this.HitPoints/(float)this.TotalHitPoints;
+        healthBar.GetComponent<Transform>().localScale = new Vector3(barHealth,1,1);
+        Debug.Log("hp:" + this.HitPoints + "%"+ this.TotalHitPoints+ " -> "+(barHealth));
     }
+
+
+    protected override void DefenceActionPerformed(){ //runs after unit has been attacked
+        ShowHealth();
+    }
+
 
 }
