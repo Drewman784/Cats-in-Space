14c45a1bde75024b199ea4a7a8d8c9c446d80239
Drew <technoman1@icloud.com>
2025-03-24 15:17:56 -0400

    Next Level Script

---
+++ Assets/Scripts/Resolvers/Cats_Domination.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+namespace TbsFramework.Grid.GameResolvers
+{
+    public class Cats_Domination : GameEndCondition
+    {
+        public SceneAsset NextLevel;
+
+        public override GameResult CheckCondition(CellGrid cellGrid)
+        {
+            var playersAlive = cellGrid.Units.Select(u => u.PlayerNumber).Distinct().ToList();
+            if (playersAlive.Count == 1)
+            {
+                var playersDead = cellGrid.Players.Where(p => p.PlayerNumber != playersAlive[0])
+                                                  .Select(p => p.PlayerNumber)
+                                                  .ToList();
+
+                // Change the scene to the one listed in the inspector
+                SceneManager.LoadScene(NextLevel.name);
+
+                return new GameResult(true, playersAlive, playersDead);
+            }
+            return new GameResult(false, null, null);
+        }
+    }
+}
+
---
+++ Assets/Scripts/Resolvers/Cats_Position.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TbsFramework.Cells;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace TbsFramework.Grid.GameResolvers
+{
+
+    public class PositionCondition : GameEndCondition
+    {
+
+        public SceneAsset NextLevel;
+
+        public Cell DestinationCell;
+        [Tooltip("Specifies whether the condition applies to any player")]
+        public bool AnyPlayer;
+        [Tooltip("Specifies a single player that the condition applies to")]
+        public int AppliesToPlayerNo;
+
+        public override GameResult CheckCondition(CellGrid cellGrid)
+        {
+            if (DestinationCell.CurrentUnits.Count > 0
+                && (DestinationCell.CurrentUnits.Exists(u => u.PlayerNumber == AppliesToPlayerNo) || AnyPlayer == true))
+            {
+                var winningPlayers = new List<int>() { DestinationCell.CurrentUnits[0].PlayerNumber };
+                var loosingPlayers = cellGrid.Players.Where(p => p.PlayerNumber != DestinationCell.CurrentUnits[0].PlayerNumber)
+                                                     .Select(p => p.PlayerNumber)
+                                                     .ToList();
+
+                // Change the scene to the one listed in the inspector
+                SceneManager.LoadScene(NextLevel.name);
+
+                return new GameResult(true, winningPlayers, loosingPlayers);
+            }
+
+            return new GameResult(false, null, null);
+        }
+    }
+}
+
