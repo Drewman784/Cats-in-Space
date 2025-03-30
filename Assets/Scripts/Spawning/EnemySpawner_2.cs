10d96ff6eb44a7cbe80a785674dab30ca46a5c1e
Drew <technoman1@icloud.com>
2025-03-30 10:53:32 -0400

    Spawning code work

--- Assets/Scripts/Spawning/EnemySpawner_2.cs
+++ Assets/Scripts/Spawning/EnemySpawner_2.cs
@@ -7,10 +7,12 @@ using UnityEngine;
 
 namespace TbsFramework.Grid.UnitGenerators
 {
-    public class CustomUnitGenerator : MonoBehaviour, IUnitGenerator
+    public class CustomUnitGenerator : MonoBehaviour
     {
-        public Transform UnitsParent;
-        public Transform CellsParent;
+        public GameObject UnitsParent;
+        public GameObject CellsParent;
+
+        public event EventHandler UnitSpawned;
 
         public int TurnCounter;
         //Specifies how many turns it takes to be spawned
@@ -22,89 +24,37 @@ namespace TbsFramework.Grid.UnitGenerators
         private int nEndTurns;
         private int nTurn;
 
-        //public event EventHandler UnitSpawned;
-        //private Unit SpawnedUnit;
-
         private void Start()
         {
-            //GetComponent<CellGrid>().TurnEnded += OnTurnEnded;
+            nTurn = 0;
         }
 
-        //private void OnTurnEnded(object sender, bool isNetworkInvoked)
-        //{
-        //    nEndTurns++;
-        //    var distinctPlayersAlive = (sender as CellGrid).Units.Select(u => u.PlayerNumber)
-        //                                                         .Distinct()
-        //                                                         .ToList().Count;
-        //    if (nEndTurns % distinctPlayersAlive == 0)
-        //    {
-        //        nTurn++;
-        //        if (nTurn >= TurnCounter)
-        //        {
-        //            nTurn = 0; // Reset the turn counter every 2 turns
-        //        }
-        //    }
-        //    return nTurn >= TurnCounter;
-//
-        //public bool CanSpawn()
-        //{
-        //    return nTurn == TurnCounter;
-        //}
-
-        //private void Update()
-        //{
-        //    if (CanSpawn() && isPositive)
-        //    {
-        //    foreach (Transform cellTransform in CellsParent)
-        //    {
-        //        var cell = cellTransform.GetComponent<Cell>();
-        //        if (cell != null && cell.IsTaken == false)
-        //        {
-        //            if (UnitsParent.childCount > 0)
-        //            {
-        //                var unitInstance = Instantiate(UnitsParent.GetChild(0).gameObject, cell.transform.position, Quaternion.identity, UnitsParent);
-        //                var unit = unitInstance.GetComponent<Unit>();
-        //                if (unit != null)
-        //                {
-        //                    unit.PlayerNumber = AppliesToPlayerNo;
-        //                    cell.IsTaken = true;
-        //                    nTurn = 0; // Reset turn counter after spawning
-        //                    break;
-        //                }
-        //            }
-        //            else
-        //            {
-        //                Debug.LogError("UnitsParent has no children to spawn.");
-        //            }
-        //        }
-        //    }
-        //    }
-        //}
-
-        /// <summary>
-        /// Returns units that are children of UnitsParent object.
-        /// </summary>
-        public List<Unit> SpawnUnits(List<Cell> cells)
+        private void Update()
         {
-            List<Unit> ret = new List<Unit>();
-            for (int i = 0; i < UnitsParent.childCount; i++)
+            if (isPositive)
             {
-                var unit = UnitsParent.GetChild(i).GetComponent<Unit>();
-                if (unit != null)
+                nTurn++;
+                if (nTurn % 2 == 0)
                 {
-                    ret.Add(unit);
-                }
-                else
-                {
-                    Debug.LogError("Invalid object in Units Parent game object");
+                    SpawnUnit();
                 }
             }
-            return ret;
         }
 
-        public void SnapToGrid()
+        private void SpawnUnit()
         {
-
+            // Implement the logic to spawn a unit here
+            // Example:
+            GameObject newUnit = new GameObject("Unit");
+            if (UnitsParent != null && UnitsParent.scene.IsValid())
+            {
+                newUnit.transform.parent = UnitsParent.transform;
+            }
+            else
+            {
+                Debug.LogError("UnitsParent is not set or is not part of a valid scene.");
+            }
+            UnitSpawned?.Invoke(this, EventArgs.Empty);
         }
     }
 }
