cbd6a1331385d3ed8804f7857327ba4db8f8ef85
caruiter <99144307+caruiter@users.noreply.github.com>
2025-04-19 21:51:36 -0400

    all units targeting same enemy catalyst
    - renamed catalyst buff to DamageTakenCatalystBuff
    - created AllTargetingCatalystBuff
    - created AllTargetingCatalyst script that checks if all units on same side are targeting the same enemy, then applies buff to psionic unit
    - added AllTargeting Catalyst script to psionic units
    
    note: AllTargetingCatalyst only activates after specified unit's action

---
+++ Assets/Scripts/Cat-Alysts/AllTargetingCatalyst.cs
@@ -0,0 +1,47 @@
+using TbsFramework.Units;
+using UnityEngine;
+
+public class AllTargetingCatalyst : BaseCatalyst
+{
+    [SerializeField] Buff buffToApply;
+    private bool hasBeenUsed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        hasBeenUsed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void CheckCatalyst()
+    {
+        if(!hasBeenUsed){
+            //Debug.Log("evaluation?" + catMon);
+            base.CheckCatalyst();
+            if(this.catMon.AllAttackingSameUnit(this.GetComponent<SampleUnit>().PlayerNumber)){
+                //Debug.Log("mmhmm");
+                if(GetComponent<SampleUnit>().PlayerNumber == 0){
+                    //Debug.Log("yeah");
+                    this.CheckActivation();
+                } else{
+                    //Debug.Log("nah");
+                    TriggerCatalystEffect();
+                }
+            }
+        }
+        //base.CheckCatalyst();
+    }
+
+    public override void TriggerCatalystEffect()
+    {
+        //Debug.Log("yay effect triggered!");
+        hasBeenUsed = true;
+        GetComponent<SampleUnit>().AddBuff(buffToApply);
+        //base.TriggerCatalystEffect();
+    }
+}
