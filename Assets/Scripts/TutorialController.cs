d88a422ad16105545071b8104dbfa40d888e9b8d
caruiter <99144307+caruiter@users.noreply.github.com>
2025-05-06 01:18:23 -0400

    tutorial added to level 1

--- Assets/Scripts/TutorialController.cs
+++ Assets/Scripts/TutorialController.cs
@@ -1,10 +1,15 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class TutorialController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] List<GameObject> theTutorials;
+    private int index;
     void Start()
     {
+        index = 0;
         
     }
 
@@ -15,7 +20,13 @@ public class TutorialController : MonoBehaviour
     }
 
     public void NextPanel(){
-
+        index++;
+        if(index < theTutorials.Count){
+            theTutorials[index-1].SetActive(false);
+            theTutorials[index].SetActive(true);
+        } else{
+            QuitTutorial();
+        }
     }
 
     public void QuitTutorial(){
